4c82f23b39613775fafb62b3cffe61f15d1a94f8
Phil Feairheller <pfeairheller@gmail.com>
2021-08-04 14:44:15 -0400

    Updating recipient

--- client/views/Recipient.js
+++ client/views/Recipient.js
@@ -3,7 +3,7 @@ let m = require("mithril");
 let Recipient = {
     view: function () {
         return m("div", {"class": "w3-container w3-margin-bottom"}, [
-            m("label", "Recipient: (Han)"),
+            m("label", "Holder: (Han)"),
             m("input[readonly=true][value=EpXprWFWmvJx4dP7CqDyXRgoigTVFwEUh6i-6jUCcoU8]", {
                 "class": "w3-input w3-border w3-light-grey",
                 "type": "text"
